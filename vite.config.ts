8add755409d69f47e76a694a3337904079e3c19c
TURKCELL\TCMSERPEK <mustafa.serpek@turkcell.com.tr>
2025-02-20 10:17:54 +0300

    design update

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('antd/')) {
-            return 'ui'
-          }
           if (id.includes('node_modules')) {
             return 'vendor'
           }
